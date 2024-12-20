c8e7b800c6be39b495c9f57adaf06023445fb775
Alejandro <element3999@gmail.com>
2024-12-20 11:33:50 -0300

    adding plataforma

--- funciones/funciones.js
+++ funciones/funciones.js
@@ -1,2 +1,16 @@
 let subt = document.getElementById('subt');
-subt.textContent = 'Subtítulo cambiado desde funciones.js';
+if(subt){
+    subt.textContent = 'Subtítulo cambiado desde funciones.js';
+}
+
+let plataforma = document.getElementById('plataforma');
+if(plataforma){
+    plataforma.addEventListener('click', () => {
+        window.location.href = './recursos/paginas/plataforma.html';
+    });
+}
+
+let volver = document.getElementById('volver');
+volver.addEventListener('click', () => {
+    window.history.back();
+});
